b6254a22fc04a37b89f272abe54f85c5d58f9429
lucavila57 <91307916+lucavila57@users.noreply.github.com>
2021-10-04 11:03:55 +0200

    Delete untitled folder/JAVA BDD TD2 directory

--- untitled folder/JAVA BDD TD2/JAVA BDD TD2/src/dao/DAO.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public interface DAO<T>{
-	public abstract T getById(int id) throws Exception;
-	public abstract boolean create(T objet) throws Exception;
-	public abstract boolean update(T objet) throws Exception;
-	public abstract boolean delete(T objet) throws Exception;	
-}
--- untitled folder/JAVA BDD TD2/JAVA BDD TD2/src/dao/Persistance.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-public enum Persistance {
-	MYSQL, NO_SQL, LISTE_MEMOIRE, XML;
-}

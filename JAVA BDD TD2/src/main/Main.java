9f9613a538c0b5fc018a813bd1a1b4bdc41bb69e
hugo <hugoreina.hr@gmail.com>
2021-10-07 13:32:55 +0200

    main test

--- JAVA BDD TD2/src/main/Main.java
+++ JAVA BDD TD2/src/main/Main.java
@@ -1,18 +1,22 @@
 package main;
 
 import java.sql.SQLException;
-
+import dao.mysql.*;
+import dao.*;
 import connexion.Connexion;
 import dao.listmemoire.*;
+import modele.metier.*;
 
 public class Main {
 	public static void main(String[] arg) throws Exception {
 
 	//Connexion laConnexion = new Connexion();
 	//Connexion.creeConnexion();	
-	
-	
-	ListeMemoireRevueDAO.getById(1);
+
+		
+	System.out.println(ListeMemoirePeriodiciteDAO.getInstance().getById(1));
+	System.out.println(ListeMemoirePeriodiciteDAO.getInstance().getById(2));
+
 	
 	
 }

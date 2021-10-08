cc54b9041dc0dcff3ac6769261c762321a318998
hugo <hugoreina.hr@gmail.com>
2021-10-08 10:41:56 +0200

    normal

--- JAVA BDD TD2/src/test/normalisation/NormalPaysTest.java
+++ JAVA BDD TD2/src/test/normalisation/NormalPaysTest.java
@@ -1,14 +1,45 @@
 package test.normalisation;
-import static org.junit.jupiter.api.Assertions.*; 
-import junit.framework.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import normalisation.*;
 
+public class NormalPaysTest {
 
-public class NormalPaysTest{
+	@Test
+	public void TestNormalPaysLuxembourg() {
+
+		assertEquals("Luxembourg", NormalisationPays.NormalPays("letzebuerg"));
+		assertEquals("Luxembourg", NormalisationPays.NormalPays("Letzebuerg"));
+
+	}
+
+	public void TestNormalPaysBelgique() {
+
+		assertEquals("Belgique", NormalisationPays.NormalPays("belgium"));
+		assertEquals("Belgique", NormalisationPays.NormalPays("Belgium"));
+
+	}
+
+	public void TestNormalPaysSuisse() {
+
+		assertEquals("Suisse", NormalisationPays.NormalPays("switzerland"));
+		assertEquals("Suisse", NormalisationPays.NormalPays("Switzerland"));
+
+	}
+
+	public void TestNormalPaysSuisse2() {
+
+		assertEquals("Suisse", NormalisationPays.NormalPays("schweiz"));
+		assertEquals("Suisse", NormalisationPays.NormalPays("Schweiz"));
+
+	}
 	
 	public void TestNormalPaysOK() {
 		
-		assertEquals("Luxembourg", NormalisationPays.NormalPays("letzebuerg"));
+		assertEquals("Bonjour", NormalisationPays.NormalPays("bonjour"));
 	}
-	
+
 }

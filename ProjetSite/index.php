88c431e4be6915d0a5679559a78231f1dd740946
lucavila57 <91307916+lucavila57@users.noreply.github.com>
2022-04-12 16:45:50 +0200

    Add files via upload

---
+++ ProjetSite/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"  content="width=device-width, initila-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="CSS/home.css">
+</head>
+<body>
+<nav class="navbar">
+<div class="nav">
+    <img src="img/dark-logo.png" class="brand-logo" alt="">
+    <div class="nav-items">
+        <div class="search">
+            <input type="text" class="search-box" placeholder="search brand, product">
+            <button class="search-btn">search</button>
+        </div>
+        <a href="#"><img src="img/user.png" alt=""></a>
+        <a href="#"><img src="img/cart.png" alt=""></a>
+    </div>
+</div>
+</nav>
+
+
+
+</body>
+</html>
